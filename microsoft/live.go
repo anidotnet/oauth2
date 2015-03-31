796bfb35db2f0ae13ccb94079db6203f12306636
Anindya Chatterjee <anidotnet@gmail.com>
2015-03-31 09:58:14 -0400

    Initial Commit

--- microsoft/live.go
+++ microsoft/live.go
@@ -2,11 +2,7 @@ package microsoft
 
 import (
     "golang.org/x/oauth2"
-    "net/url"
-    "net/http"
-    "bytes"
-    "net/http/httputil"
-    "fmt"
+    "errors"
 )
 
 var Endpoint = oauth2.Endpoint {
@@ -17,7 +13,7 @@ var Endpoint = oauth2.Endpoint {
 
 // Destroys both access_token and refresh_token of Live
 func Revoke(config *oauth2.Config, token *oauth2.Token) error {
-    var buf bytes.Buffer
+    /*var buf bytes.Buffer
     buf.WriteString(config.Endpoint.RevokeURL)
     buf.WriteByte('?')
     v := url.Values {
@@ -44,5 +40,6 @@ func Revoke(config *oauth2.Config, token *oauth2.Token) error {
         return err
     }
     fmt.Printf("%s", data)
-    return nil
+    return nil*/
+    return errors.New("Not implemented, due to https://github.com/OneDrive/onedrive-api-docs/issues/58")
 }
